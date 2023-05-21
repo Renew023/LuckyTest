aa10661b5b8077be3793e7b97d5f4f6a70053050
Renew023 <ya1539@naver.com>
2023-05-21 17:02:20 +0900

    Text 파일 만들기 공부
    https://jungmonster.tistory.com/202

---
+++ LuckyTest/Assets/Scripts/UI_Component/UI_MouseOverEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_MouseOverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+
+{
+    public GameObject objSubMenu;
+    public Button btnMenu;
+
+    void Start(){
+        objSubMenu.SetActive(false);
+        btnMenu.onClick.AddListener(ToggleSubMenu);
+        Debug.Log("4");
+    }
+
+    void ToggleSubMenu(){
+        objSubMenu.SetActive(!objSubMenu.activeSelf);
+        Debug.Log("3");
+    }
+    public void OnPointerEnter(PointerEventData eventData){
+        objSubMenu.SetActive(true);
+        Debug.Log("2");
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        objSubMenu.SetActive(false);
+        Debug.Log("1");
+    }
+    
+}
