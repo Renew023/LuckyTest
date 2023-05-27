e0ee819137ff27905e1463001102e57cfac9f2f8
Renew023 <ya1539@naver.com>
2023-05-27 23:12:55 +0900

    유니티 강의
    https://www.youtube.com/@unitynote
    https://www.youtube.com/watch?v=wBzAe9Ewsc4

---
+++ LuckyTest/Assets/Scripts/UI_Component/TextReading.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TextReading : MonoBehaviour
+{
+    void Start()
+    {
+        int speed;
+        string b;
+        b = speed * 0.8;
+        FileStream test = new FileStream("C:/Users/CKIRUser/Downloads/memo.txt", FileMode.Create);
+
+        StreamWriter testStreamWriter = new StreamWriter(test);
+        testStreamWriter.Write("Stream Writer 테스트 중");
+        testStreamWriter.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
